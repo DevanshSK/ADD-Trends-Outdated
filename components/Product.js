8d2d07a70d4ed7be5fa5b4308f255b61461d5ca5
DevanshSK <devanshs843@gmail.com>
2022-10-17 22:32:12 +0530

    Card styles updated

--- components/Product.js
+++ components/Product.js
@@ -1,14 +1,19 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { AiFillStar } from "react-icons/ai";
+import { useStateContext } from "../lib/context";
 
 export default function Product({ product }) {
+  const { formattedNumber } = useStateContext();
+
   console.log(product);
-  const { title, brand, rating, id, price, images, discountPercentage } =
+  const { title, brand, rating, id, price, images, discountPercentage, stock } =
     product;
 
   const calculatePrice = (discountprice, discount) => {
     let remPercentage = 100 - discount;
-    return Number.parseInt((discountprice / remPercentage) * 100 * 80);
+    let n = Number.parseInt((discountprice / remPercentage) * 100 * 80);
+    return formattedNumber(n);
   };
 
   return (
@@ -23,37 +28,45 @@ export default function Product({ product }) {
       }}
       whileTap={{ scale: 0.9 }}
       transition={{ duration: 0.3 }}
-      className=" max-w-[280px] sm:max-w-[340px] justify-self-center shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] bg-[linear-gradient(145deg,_#ececec,_#ffffff)] rounded-xl mt-10 px-5"
+      className=" max-w-[280px] sm:max-w-[340px] justify-self-center shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] bg-[linear-gradient(145deg,_#ececec,_#ffffff)] rounded-2xl mt-10 px-5"
     >
       <Link href={`/products/${id}`}>
         <div>
-          <div className="aspect-square py-5">
+          <div className="aspect-[3/4] my-5  relative">
             <img
               src={images[0]}
               alt=""
-              className="w-full h-full object-cover object-center shadow rounded-md shadow-[rgba(0,0,0,0.15)_1.95px_1.95px_2.6px;]"
+              className="w-full h-full object-cover object-center shadow rounded-xl shadow-[rgba(0,0,0,0.15)_1.95px_1.95px_2.6px;]"
             />
+            <div className=" flex items-center gap-1 px-1 py-[2px] rounded-sm w-max absolute bottom-3 left-3      bg-[hsla(0,0%,100%,.8)] text-sm ">
+              <span>{rating}</span>
+              <AiFillStar className="text-red-500" />
+              <span>| {formattedNumber(stock)}</span>
+            </div>
           </div>
           <div className="px-3 pb-4">
             <h3 className="text-2xl main-font">{title}</h3>
-            <h4 className="text-base font-semibold text-gray-500 my-1">
+            <h4 className="text-base font-medium text-gray-500 my-1">
               {brand}
             </h4>
             {/* <h2>{rating}</h2> */}
-            <div className="flex gap-x-2 items-baseline">
-              <h4 className="text-2xl font-bold text-[#0d0d25]">
+            <div className="flex gap-x-[6px] items-baseline">
+              <h4 className="text-lg font-semibold text-[#0d0d25]">
                 &#8377;{price * 80}
               </h4>
-              <h4 className="text-xl font-semibold text-gray-500 line-through">
+              <p className="text-md font-medium text-gray-500 line-through">
                 &#8377;{calculatePrice(price, discountPercentage)}
-              </h4>
+              </p>
+              <p className="text-md font-medium text-red-500 ">
+                ({Number.parseInt(discountPercentage)}% OFF)
+              </p>
             </div>
           </div>
         </div>
       </Link>
-      <button className="border-2 border-black w-full block py-2 mb-5 text-white bg-[#0d0d25] rounded">
+      {/* <button className="border-2 border-black w-full block py-2 mb-5 text-white bg-[#0d0d25] rounded">
         Add To Cart
-      </button>
+      </button> */}
     </motion.div>
   );
 }
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -6,7 +6,11 @@ const ProductDetails = () => {
   const { query } = useRouter();
   // console.log("The value of query is : ", query.id);
 
-  return <div>The ID of the product is {query.id}</div>;
+  return (
+    <section className="bg-[linear-gradient(120deg,_#fdfbfb_0%,_#ebedee_100%)] min-h-screen">
+      <div className="container py-24 mx-auto px-3"></div>
+    </section>
+  );
 };
 
 export default ProductDetails;
